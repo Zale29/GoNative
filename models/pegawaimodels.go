4ce80185db374f7fba1bb2570003b91d04910eb9
Zale29 <107763340+Zale29@users.noreply.github.com>
2023-11-04 09:49:46 +0700

    update

--- models/pegawaimodels.go
+++ models/pegawaimodels.go
@@ -113,3 +113,38 @@ func UpdatePegawai(id int, nama string, alamat string, telepon string) (Response
 
 	return res, nil
 }
+
+func DeletePegawai(id int) (Response, error) {
+	var res Response
+
+	con := db.CreateCon()
+
+	sqlStatement := "DELETE FROM pegawai WHERE id = ?"
+
+	stmt, err := con.Prepare(sqlStatement)
+
+	if err != nil {
+		return res, err
+	}
+
+	result, err := stmt.Exec(id)
+
+	if err != nil {
+		return res, err
+	}
+
+	RowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return res, err
+	}
+
+	res.Status = http.StatusOK
+	res.Message = "Success"
+	res.Data = map[string]int64{
+		"rows_affected": RowsAffected,
+	}
+
+	return res, err
+
+}
